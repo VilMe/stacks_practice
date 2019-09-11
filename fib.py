aee1f9fa2d0dadca0e80a58b9214abffd65f33f8
Vilma M <vilma.medrano00@gmail.com>
2019-09-11 11:18:48 -0700

    print something n times ascending in one function and print descending n times in another function, full circle

--- fib.py
+++ fib.py
@@ -1,26 +1,31 @@
 
 
 
-# # def print_somthing_n_times(something, n_times):
-#     """ this function goes from n times to zero, descendgin
+def print_somthing_n_times(something, n_times):
+    """ this function goes from n times to zero, descendgin
+    
+    """
+    while n_times > 0:
+        print(something, n_times)
+        return print_somthing_n_times(something, n_times - 1)
+    return
 
-#     """
-#     while n_times > 0:
-#         print(something, n_times)
-#         return fib(something, n_times - 1)
 
-
-
-# fib('hhhhhell world', 4)
+print_somthing_n_times('hhhhhell world', 4)
 
 
 
 
 def print_something_ascending(something, n_times, start = 0):
     """ print something n_times in ascending order!
-
+        recusively, 
+        can set defualt number can be changed,
+        i don't handle if start is greater than n_times, run time error |(or) stack overflow
     """
-    while start != n_times:
+    while start <= n_times: #can put either !=, <, <= depending on whether you want
+    #n_times to be inclusive or exclusive
         print(something, start)
         return print_something_ascending(something, n_times, start + 1)
+
+    return
 print_something_ascending('hhhhhell world', 4)
