bc9472bdd3a5498db28039b0c6ca787ae07b3ec5
Vilma M <vilma.medrano00@gmail.com>
2019-09-11 11:07:16 -0700

    define recursive print something ascending function, will find doc test and implement for each function

--- fib.py
+++ fib.py
@@ -1,11 +1,26 @@
 
 
 
-def fib(something, n_times):
-    while n_times > 0:
-        print(something, n_times)
-        return fib(something, n_times - 1)
+# # def print_somthing_n_times(something, n_times):
+#     """ this function goes from n times to zero, descendgin
+
+#     """
+#     while n_times > 0:
+#         print(something, n_times)
+#         return fib(something, n_times - 1)
 
 
 
-fib('hhhhhell world', 4)
+# fib('hhhhhell world', 4)
+
+
+
+
+def print_something_ascending(something, n_times, start = 0):
+    """ print something n_times in ascending order!
+
+    """
+    while start != n_times:
+        print(something, start)
+        return print_something_ascending(something, n_times, start + 1)
+print_something_ascending('hhhhhell world', 4)
